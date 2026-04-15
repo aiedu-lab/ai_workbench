766e4e188224b4b668ac48010e4306f09aee24d2
Arijit Sarcar <asarcar@gmail.com>
2026-04-14 17:38:22 -0700

    feat: added buggy code to test code review via GitHub actions

--- projects/code_review/code_with_bug.py
+++ projects/code_review/code_with_bug.py
@@ -2,19 +2,21 @@
 
 def divide(a, b):
   """Utility function to divide a by b."""
-  return a / b
+  return (a/b)
 
 def get_users(users, id):
   """Utility function to get a user by ID."""
-  return users[id]
+  return (users[id])
 
 def main():
   a = 10
   b = 0
-  print(divide(a, b))
+  res1 = divide(a, b)
+  print(res1)
 
   users = {1: "Alice", 2: "Bob"}
-  print(get_users(users, 3))
+  res2 = get_users(users, 3)
+  print(res2)
 
 if __name__ == "__main__":
   main()
